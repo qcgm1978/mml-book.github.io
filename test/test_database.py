da98fa55e4f75e04f05777d8e51210a50d4b62d4
Philipp <qcgm197874@gmail.com>
2020-10-30 23:36:47 +0800

    definition database

--- test/test_database.py
+++ test/test_database.py
@@ -1,8 +1,9 @@
 import unittest,mysql
 from test.mysql_op import MysqlOp
+from test.definitions import noun
 class TDD_TEST_MYSQL(unittest.TestCase):
     def setUp(self):
-        self.m = MysqlOp('customers',{'name': 'VARCHAR(255)', 'address': 'VARCHAR(255)'})
+        self.m = MysqlOp('mml',{'noun': 'VARCHAR(255)', 'meaning': 'VARCHAR(510)'})
         return super().setUp()
     def test_key(self):
         self.assertRaises(mysql.connector.errors.ProgrammingError, lambda: self.m.createPrimaryKey())
@@ -11,31 +12,17 @@ class TDD_TEST_MYSQL(unittest.TestCase):
         except mysql.connector.errors.ProgrammingError:
             pass
     def test_unique(self):
-        self.assertIsNone(self.m.unique('address'))
+        self.assertIsNone(self.m.unique('noun'))
     def test_insert(self):
-        val = ("John", "Highway 21")
+        val = ("Gaussian elimination", "an algorithm that performs elementary transformations to bring a system of linear equations into reduced row-echelon form")
         r=self.m.insertInto(val)
         self.assertEqual(self.m.mycursor.rowcount, 1 if r else -1)
-        val = [
-        ('Peter', 'Lowstreet 4'),
-        ('Amy', 'Apple st 652'),
-        ('Hannah', 'Mountain 21'),
-        ('Michael', 'Valley 345'),
-        ('Sandy', 'Ocean blvd 2'),
-        ('Betty', 'Green Grass 1'),
-        ('Richard', 'Sky st 331'),
-        ('Susan', 'One way 98'),
-        ('Vicky', 'Yellow Garden 2'),
-        ('Ben', 'Park Lane 38'),
-        ('William', 'Central st 954'),
-        ('Chuck', 'Main Road 989'),
-        ('Viola', 'Sideway 1633')
-        ]
+        val =  [(k, v) for k, v in noun.items()]
         r=self.m.executemany(val)
         self.assertEqual(self.m.mycursor.rowcount, len(val) if r else -1)
     def test_insert_id(self):
-        sql = "INSERT INTO customers (name, address) VALUES (%s, %s)"
-        val = ("Michelle", "Blue Village")
+        sql = "INSERT INTO mml (noun, meaning) VALUES (%s, %s)"
+        val = ("B=A⊤", "For A ∈ Rm×n the matrix B ∈ Rn×m with bij =aji iscalled the transpose of A")
         self.m.insertInto( val)
         self.assertIsInstance( self.m.mycursor.lastrowid,int)
 if __name__ == '__main__':
